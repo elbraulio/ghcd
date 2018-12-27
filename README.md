59cd8d34a0104fc4af3b8d7ed5800c0752a824ac
elbraulio <brauliop.3@gmail.com>
2018-12-27 18:55:44 -0300

    first deploy script

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # ghcd
-continuous delivery for Github projects
+continuous delivery for Github projects. This supports __only mvn projects__.
+
+# How to 
+
+All you need to change is in the beginning of the `deploy.sh` file. Here is:
+
+```bash
+# ========= <CONFIG =========
+... all variables to change are here
+# ========= CONFIG> =========
+```
+
+## variables
+
+| name                 | description                                                  | example                                 |
+| :------------------- | ------------------------------------------------------------ | --------------------------------------- |
+| repo_url             | url to clone git repo.                                       | `https://github.com/elbraulio/ghcd.git` |
+| repo_name            | repo name that is included in `repo_url`.                    | `ghcd`                                  |
+| repo_branch          | branch that you want to deploy.                              | `master`                                |
+| war_name             | name with which the war will be deployed.                    | `dev-api`                               |
+| configuration_folder | contains the configurations files that will replace the original repo configuration files. | `/opt/configs/project1/config`          |
+| deploy_folder        | path to the folder where the war should be copied.           | `/opt/tomcat/webapps`                   |
+| server_refresh       | the time that takes your server to refresh and deploy a new change. With a __s__ at the end. | `10s` (__with a 's' at the end__)       |
+
---
+++ deploy.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+# ========= <CONFIG =========
+# git url to clone repo.
+repo_url=required*
+# repo name, MUST be the same with the name in repo_url.
+repo_name=required*
+# branch with the code that will be deployed.
+repo_branch=required*
+# war name to be deployed.
+war_name=required*
+# this will replace the original configuration from repo
+configuration_folder=required*
+# server folder to be deployed
+deploy_folder=required*
+# server refresh rate
+server_refresh=10s
+# ========= CONFIG> =========
+build_folder=deploying_war
+build_root=$(pwd)
+# make folder for deploying
+echo "trying to remove older and unused folders"
+rm -r $build_folder
+mkdir $build_folder
+# clone repo
+echo "cloning repo $repo_url"
+cd $build_folder
+git clone -b $repo_branch --single-branch $repo_url
+if [[ $? != 0 ]] ; then
+    cd $build_root
+    rm -r $build_folder
+    exit 1
+fi
+cd $build_root
+# replace configuration
+rm -r $build_folder/$repo_name/src/main/resources/*
+if [[ $? != 0 ]] ; then
+    cd $build_root
+    rm -r $build_folder
+    exit 1
+fi
+cp -a $configuration_folder/. $build_folder/$repo_name/src/main/resources/
+if [[ $? != 0 ]] ; then
+    cd $build_root
+    rm -r $build_folder
+    exit 1
+fi
+# create a build
+echo "creating build"
+cd $build_folder/$repo_name
+mvn clean install
+if [[ $? != 0 ]] ; then
+    cd $build_root
+    rm -r $build_folder
+    exit 1
+fi
+# deploy build
+echo "deploying"
+rm $deploy_folder/$war_name
+# waiting server
+echo "waiting for server to take changes. $server_refresh"
+sleep $server_refresh
+cp target/*.war $deploy_folder/$war_name
+cd $build_root
+rm -r $build_folder
+echo "DEPLOYED"
